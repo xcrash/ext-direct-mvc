8f86fa860f168f50190c6d638a9d2b8f43f4f02e
elishnevsky <elishnevsky@59ee3dda-d968-11de-b113-f7137c4feea6>
2010-01-22 19:54:53 +0000

    New structure. All assemblies are included, so ASP.NET MVC doesn't need to be installed. Support for ASP.NET MVC 2.0.
    git-svn-id: http://ext-direct-mvc.googlecode.com/svn/trunk@32 59ee3dda-d968-11de-b113-f7137c4feea6

---
+++ Ext.Direct.Mvc.Demo/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Ext.Direct.Mvc.Demo.Controllers {
+
+    public class HomeController : Controller {
+
+        [DirectIgnore]
+        [HandleError]
+        public ActionResult Index() {
+            return View();
+        }
+    }
+}
---
+++ Ext.Direct.Mvc.Demo/Source/App.js
@@ -0,0 +1,81 @@
+﻿Ext.onReady(function() {
+    Ext.BLANK_IMAGE_URL = 'http://extjs.cachefly.net/ext-3.0.3/resources/images/default/s.gif';
+    Ext.Direct.addProvider(Ext.app.REMOTING_API);
+    Ext.QuickTips.init();
+    
+    new Ext.Container({
+        renderTo: document.body,
+        height: 300,
+        layout: {
+            type: 'hbox',
+            padding: '5',
+            align: 'stretch',
+            defaultMargins: {top:5,right:5,bottom:0,left:5}
+        },
+        items: [{
+            xtype: 'simpletestpanel',
+            width: 510
+        }, {
+            xtype: 'formtestpanel',
+            width: 310
+        }, {
+            xtype: 'fileuploadpanel',
+            width: 310
+        }]
+    });
+    
+    new Ext.Container({
+        renderTo: document.body,
+        height: 300,
+        layout: {
+            type: 'hbox',
+            padding: '5',
+            align: 'stretch',
+            defaultMargins: {top:0,right:5,bottom:5,left:5}
+        },
+        items: [{
+            xtype: 'treetestpanel',
+            width: 350
+        }, {
+            xtype: 'employeegrid',
+            width: 790
+        }]
+    });
+    
+    Ext.Direct.on('exception', function(e) {
+        var title = 'Direct Exception', message;
+        
+        if (Ext.isDefined(e.where)) {
+            // Detailed error message for developer
+            message = String.format('<b>{0}</b><p>The exception was thrown from {1}.{2}()</p><pre>{3}</pre>', Ext.util.Format.nl2br(e.message), e.action, e.method, e.where);
+            var w = new Ext.Window({
+                title: title,
+                width: 600,
+                height: 400,
+                modal: true,
+                layout: 'fit',
+                border: false,
+                maximizable: true,
+                items: {
+                    html: message,
+                    autoScroll: true,
+                    preventBodyReset: true,
+                    bodyStyle: 'font-size:12px',
+                    padding: 5
+                },
+                buttons: [{
+                    text: 'OK',
+                    handler: function() {
+                        w.close();
+                    }
+                }],
+                buttonAlign: 'center',
+                defaultButton: 0
+            }).show();                            
+        } else {
+            // User friendly message for end user
+            message = 'Error occured. Unable to process request.';
+            Ext.Msg.alert(title, message);
+        }
+    });
+});
